3941c712df0b3ac5b58c2dbce5bb5b6a8782c5ac
loraiv <102684937+loraiv@users.noreply.github.com>
2025-06-17 00:15:33 +0300

    working pytests

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -2,7 +2,7 @@ import pytest
 from app import app, db, User
 from flask import json
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def client():
     app.config['TESTING'] = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
